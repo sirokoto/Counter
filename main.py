2af87c19f493545f44745caf454b399587faad88
Sirokoto <ruslanplat48@mail.ru>
2025-04-26 03:09:22 +0700

    Changed exception, changed print lines

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-cc = 1
-file_name = 'list.txt'
+counter: int = 1
+file_name: str = 'list.txt'
 try:
     file = open(file_name, 'r')
     for line in file.readlines():
-        print(f'{cc:}. {line:^20}')
-        cc+=1
-except FileNotFoundError:
-    raise FileNotFoundError("File not found")
+        print(f'{counter}. {line}', end='')
+        counter+=1
+except Exception as e:
+    #raise FileNotFoundError("File not found")
+    print(f"Error: {e}")
